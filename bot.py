c63b0c5efa0cb9229c7130c2735b49ddc88feca0
lvl99hegde <rajendrahegde2002@gmail.com>
2025-12-19 16:54:46 +0530

    Reorganize imports and clean up code structure

--- bot.py
+++ bot.py
@@ -1,23 +1,17 @@
 import discord
 from discord.ext import commands
-import os  # Move import to top
+import os
 
-# Enable intents
 intents = discord.Intents.default()
 intents.message_content = True
-
-# Create bot instance
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Event: Bot is ready
 @bot.event
 async def on_ready():
     print("HootBot is online")
 
-# Command: !hoot
 @bot.command()
 async def hoot(ctx):
     await ctx.send("hoot")
 
-# Run the bot using the token from environment variable
 bot.run(os.getenv("TOKEN"))
